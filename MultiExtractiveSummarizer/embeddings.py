7d99491924579c4838fa46d4409f0a93e87c88a4
iamarsh <arshraj.randhawa@gmail.com>
2024-07-07 14:20:40 +0530

    updated

--- MultiExtractiveSummarizer/embeddings.py
+++ MultiExtractiveSummarizer/embeddings.py
@@ -1,14 +1,20 @@
 from sentence_transformers import SentenceTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 class Embeddings:
     def __init__(self, method):
         self.method = method
         if method == 'sbert':
             self.model = SentenceTransformer('all-MiniLM-L6-v2')
+        elif method == 'tfidf':
+            self.model = TfidfVectorizer()
         else:
             raise ValueError("Model not found")
+        
 
 
     def fit_transform(self, texts):
         if self.method == 'sbert':
             return self.model.encode(texts)
+        elif self.method == 'tfidf':
+            return self.model.fit_transform(texts).toarray()
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='MultiExtractiveSummarizer',
-    version='0.2.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
         "nltk",
